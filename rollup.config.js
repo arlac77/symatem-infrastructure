7e1d59dbd7c312c6814fae2961328ec99a82a760
Markus Felten <Markus.Felten@gmx.de>
2016-12-23 22:30:27 +0100

    chore(rollup): remove rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
 /* jslint node: true, esnext: true */
 'use strict';
 
-import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   banner: '#!/usr/bin/env node',
   format: 'cjs',
-  plugins: [nodeResolve(), commonjs()]
+  plugins: [commonjs()]
 };
