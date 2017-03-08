ca72d96b659b58aa2f6094e4d855fee6dcdc3d26
Markus Felten <Markus.Felten@gmx.de>
2017-03-08 15:18:02 +0100

    feat: use caporal for cmd-line parsing

--- src/module.js
+++ src/module.js
@@ -5,7 +5,7 @@
 const symatem = require('symatem'),
   path = require('path'),
   fs = require('fs'),
-  program = require('commander');
+  program = require('caporal');
 
 import {
   generateComputers
@@ -17,50 +17,50 @@ import {
 }
 from './dns';
 
-//require('pkginfo')(module, 'version');
-
 program
   .description('infrastructure data utility')
-  //  .version(module.exports.version)
+  .version(require(path.join(__dirname, '..', 'package.json')).version)
   .option('--store <file>', 'symatem store')
   .option('--hrl <file>', 'hrl source')
   .option('--out <dir>', 'output directory')
-  .parse(process.argv);
+  .action((args, options, logger) => {
+    const out = options.out || 'out';
 
-const out = program.out || 'out';
+    process.on('uncaughtException', err => logger.error(err));
+    process.on('unhandledRejection', reason => logger.error(reason));
+    symatem.open({
+      store: options.store
+    }).then(connection => connection.upload(fs.readFileSync(options.hrl))
+      .then(() => connection.upload('networkInterface')
+        .then(result => connection.query(false, symatem.queryMask.VMV, 0, result[0], 0)
+          .then(symbols => symbols.map(symbol => connection.decodeSymbolWithCache(symbol)))
+          .then(dps => Promise.all(dps))
+          .then(decoded => {
+            const promises = [];
 
-process.on('uncaughtException', err => console.error(err));
-process.on('unhandledRejection', reason => console.error(reason));
+            promises.push(connection.upload('network')
+              .then(result => connection.query(false, symatem.queryMask.VIM, 0, 0, result[0])
+                .then(symbols => symbols.map(symbol => connection.decodeSymbolWithCache(symbol)))
+                .then(dps => Promise.all(dps))
+                .then(([network]) => Promise.all([
+                  generateComputers(out, decoded, network),
+                  generateZones(out, decoded, network)
+                ]))));
 
-symatem.open({
-  store: program.store
-}).then(connection => connection.upload(fs.readFileSync(program.hrl))
-  .then(() => connection.upload('networkInterface')
-    .then(result => connection.query(false, symatem.queryMask.VMV, 0, result[0], 0)
-      .then(symbols => symbols.map(symbol => connection.decodeSymbolWithCache(symbol)))
-      .then(dps => Promise.all(dps))
-      .then(decoded => {
-        const promises = [];
-
-        promises.push(connection.upload('network')
-          .then(result => connection.query(false, symatem.queryMask.VIM, 0, 0, result[0])
-            .then(symbols => symbols.map(symbol => connection.decodeSymbolWithCache(symbol)))
-            .then(dps => Promise.all(dps))
-            .then(([network]) => Promise.all([
-              generateComputers(out, decoded, network),
-              generateZones(out, decoded, network)
-            ]))));
-
-        Promise.all(promises)
-          .then(() => {
-            connection.close();
-            console.log('done');
+            Promise.all(promises)
+              .then(() => {
+                connection.close();
+                logger.log('done');
+              })
+              .catch(e => {
+                connection.close();
+                logger.error(e);
+              });
           })
-          .catch(e => {
-            connection.close();
-            console.error(e);
-          });
-      })
-    )
-  )
-  .catch(error => console.error(error)));
+        )
+      )
+      .catch(error => logger.error(error)));
+  });
+
+program
+  .parse(process.argv);
