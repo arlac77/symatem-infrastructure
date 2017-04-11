40df9e60c8bb28a06221893bc485196e031d125e
Markus Felten <Markus.Felten@gmx.de>
2017-04-11 21:47:12 +0200

    test: use ava

--- tests/simple_test.js
+++ tests/simple_test.js
@@ -1,23 +1,18 @@
-/* global describe, it, xit, before, beforeEach, after, afterEach */
 /* jslint node: true, esnext: true */
 
 'use strict';
 
-const chai = require('chai'),
-  assert = chai.assert,
-  expect = chai.expect,
-  should = chai.should();
+import test from 'ava';
 
 const path = require('path'),
   fs = require('fs'),
   spawn = require('child_process').spawn,
   symatem = require('symatem');
 
-describe('connection problems', () => {
-  it('should fail', () =>
-    symatem.open({
+
+test('connection problems', t => {
+  symatem.open({
       store: path.join(__dirname, 'a.store')
     })
-    .catch(e => assert.equal(e.code, 'ECONNREFUSED'))
-  );
+    .catch(e => t.deepEqual(e.code, 'ECONNREFUSED'));
 });
