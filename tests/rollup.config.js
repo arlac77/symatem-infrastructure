bbe17e5bbb348476d4040453f577acfad881bfe5
Markus Felten <Markus.Felten@gmx.de>
2017-12-06 21:21:24 +0100

    chore(rollup): use rollup-plugin-multi-entry

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,20 +1,12 @@
-import babel from 'rollup-plugin-babel';
+import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
-  input: 'tests/simple-test.js',
+  input: 'tests/**/*-test.js',
   external: ['ava'],
-
-  plugins: [
-    babel({
-      babelrc: false,
-      presets: ['stage-3'],
-      exclude: 'node_modules/**'
-    })
-  ],
-
+  plugins: [multiEntry()],
   output: {
-    file: 'build/test-bundle.js',
     format: 'cjs',
+    file: 'build/bundle-test.js',
     sourcemap: true
   }
 };
