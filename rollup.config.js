4c403329a95153e1de6fafe7f197b3dcec1522df
Markus Felten <Markus.Felten@gmx.de>
2018-09-13 16:21:51 +0200

    fix(rollup): generate node call with --experimental-modules --experimental-worker

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,28 @@
-import cleanup from 'rollup-plugin-cleanup';
-import executable from 'rollup-plugin-executable';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import pkg from './package.json';
+import cleanup from "rollup-plugin-cleanup";
+import executable from "rollup-plugin-executable";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import pkg from "./package.json";
 
 export default {
   output: {
-    file: pkg.bin['sym-infra-util'],
-    format: 'cjs',
-    banner: '#!/usr/bin/env node'
+    file: pkg.bin["sym-infra-util"],
+    format: "cjs",
+    banner: "#!/usr/bin/env node --experimental-modules --experimental-worker",
+    interop: false
   },
-  plugins: [commonjs(), json()],
+  plugins: [resolve(), commonjs(), json(), cleanup(), executable()],
   external: [
-    'symatem',
-    'plist',
-    'commander',
-    'dns-zonefile',
-    'mkdirp',
-    'path',
-    'fs',
-    'pkginfo',
-    'SymatemJS'
+    "symatem",
+    "plist",
+    "commander",
+    "dns-zonefile",
+    "mkdirp",
+    "path",
+    "fs",
+    "pkginfo",
+    "SymatemJS"
   ],
   input: pkg.module
 };
