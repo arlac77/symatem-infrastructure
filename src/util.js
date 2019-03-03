743277d1249b0d0d08a2c7ce4fc5bfff5583cab0
Markus Felten <Markus.Felten@gmx.de>
2019-03-03 21:03:07 +0100

    refactor: use fs.promises.mkdir instead of makeDir

--- src/util.js
+++ src/util.js
@@ -1,13 +1,8 @@
-const path = require('path');
-const fs = require('fs');
-const makeDir = require('make-dir');
-
-const {
-  promisify
-} = require('util');
-
-const _writeFile = promisify(fs.writeFile);
+const { join } = require("path");
+const fs = require("fs");
 
 export function writeFile(dir, name, data, encoding) {
-  makeDir(dir).then(path => _writeFile(path.join(dir, name), data, encoding));
+  fs.promises
+    .mkdir(dir, { recursive: true })
+    .then(path => fs.promises.writeFile(join(dir, name), data, encoding));
 }
