834bbec7d4ce9afeb3469718a270b27083d2e916
Markus Felten <Markus.Felten@gmx.de>
2019-11-15 12:01:35 +0100

    chore(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,61 @@
-import builtins from "builtin-modules";
-import cleanup from "rollup-plugin-cleanup";
-import executable from "rollup-plugin-executable";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import json from "rollup-plugin-json";
+import executable from "rollup-plugin-executable";
+import json from "@rollup/plugin-json";
+import cleanup from "rollup-plugin-cleanup";
+import builtins from "builtin-modules";
 import pkg from "./package.json";
 
-export default {
-  output: {
-    file: pkg.bin["sym-infra-util"],
+const external = [...builtins,  "symatem",
+"plist",
+"commander",
+"dns-zonefile",
+"mkdirp",
+"path",
+"fs",
+"pkginfo",
+"SymatemJS"];
+
+const extensions = ["js", "mjs", "jsx", "tag"];
+const plugins = [
+  commonjs(),
+  resolve(),
+  json({
+    preferConst: true,
+    compact: true
+  }),
+  cleanup({
+    extensions
+  })
+];
+
+const config = Object.keys(pkg.bin || {}).map(name => {
+  return {
+    input: `src/${name}-cli.mjs`,
+    output: {
+      plugins: [executable()],
+      banner:
+        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+      file: pkg.bin[name]
+    }
+  };
+});
+
+if (pkg.module !== undefined && pkg.main !== undefined && pkg.module != pkg.main) {
+  config.push({
+    input: pkg.module,
+    output: {
+      file: pkg.main
+    }
+  });
+}
+
+export default config.map(c => {
+  c.output = {
+    interop: false,
+    externalLiveBindings: false,
     format: "cjs",
-    banner: "#!/usr/bin/env node --experimental-modules --experimental-worker",
-    interop: false
-  },
-  plugins: [resolve(), commonjs(), json(), cleanup(), executable()],
-  external: [
-    "symatem",
-    "plist",
-    "commander",
-    "dns-zonefile",
-    "mkdirp",
-    "path",
-    "fs",
-    "pkginfo",
-    "SymatemJS"
-  ],
-  input: pkg.module
-};
+    ...c.output
+  };
+  return { plugins, external, ...c };
+});
